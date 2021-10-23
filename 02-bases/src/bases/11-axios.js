3f070ec9725b6b537c306998d656401a26176f0c
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2021-10-24 00:35:08 +0200

    subida se sc2 - 11 axios gif

---
+++ 02-bases/src/bases/11-axios.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+const apiKey  = 'NMNTG1CQqPFfZe6LyU5kHtpBgCXA9Xe3hgQDS91KLZxv53'
+const url     = 'https://api.giphy.com/v1/gifs'
+
+
+const giphyApi = axios.create({
+    baseURL:url,
+    params: {
+        apiKey,
+    }
+})
+
+
+giphyApi.get('/random')
+    .then(resp => {
+        //console.log(resp.data.data.images.original.url);
+        const {data} = resp.data
+        const {url} = data.images.original
+        const img = document.createElement('img')
+        img.src = url
+        document.body.append(img)
+        //const url = resp.data.data.images.original.url
+        //
+        //const img = document.createElement('img')
+        //img.src = url
+        //
+        //document.body.append(img)
+        
+    })
