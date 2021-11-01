9420d19c4e5419639f92ba80824e46e313c6c72a
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2021-11-01 23:33:20 +0100

    Subiendo app SC5: IndecisionApp

---
+++ 05-IndecisionApp/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Counter />
+    
+  </div>
+</template>
+
+<script>
+import Counter from "@/components/Counter";
+
+export default {
+  name: "App",
+  components: {
+    Counter
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
---
+++ 05-IndecisionApp/src/components/Counter.vue
@@ -0,0 +1,80 @@
+<template>
+    <div>
+       <h2> {{customTitle }}</h2>
+       <p> {{counter + start}} <sup>2</sup> = {{squareCounterValue}} </p>
+       <button @click="increase">+ 1</button>
+       <button @click="decrease">- 1</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Contador de Numeros',
+    props: {
+        title: String,
+        start:{
+            type:Number,
+            default:0,
+            // required:true
+            validator(value){
+                return value >= 0
+            }
+        }
+    } ,
+
+    data(){
+        return {
+            counter:this.start
+        }
+    },
+    methods: { 
+        getSquareValue(){
+            const multiploDos = this.counter * this.counter
+            return multiploDos 
+        },
+        increase(){
+             this.counter = this.counter + 1
+            
+        },
+        decrease(){
+             this.counter = this.counter - 1
+            
+        }
+    },
+    computed: {
+        squareCounterValue(){
+            const multiploDos = this.counter * this.counter
+            return multiploDos
+            
+        },
+        customTitle(){
+           return this.title || 'Counter'
+           
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+button{
+    background-color: #64BB87;
+    border: 1px solid white;
+    border-radius: 5px;
+    color: black;
+    cursor: pointer;
+    font-size: 16px;
+    margin: 0 5px;
+    padding: 2%;
+    transition: 0.3s ease-in-out;
+
+}
+button:hover{
+    border-radius: 2px solid white;
+    color: azure;
+    font-size: 32px;
+    padding: 3%;
+    transition: 0.3s ease-in-out;
+    
+}
+</style>
