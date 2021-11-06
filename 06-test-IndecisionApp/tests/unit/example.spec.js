f9670927557176289ff7ea96438c28f22e0ccbed
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2021-11-06 12:38:50 +0100

    the first unit test component is valid

--- 06-test-IndecisionApp/tests/unit/example.spec.js
+++ 06-test-IndecisionApp/tests/unit/example.spec.js
@@ -1,15 +1,14 @@
 describe("example Component", () => {
   test("Debe de ser > a 10", () => {
-    ///AAA
-    //Arreglar
-    const value = 5;
+    // Arreglar
+    let value = 10
+    
     // Estimulo
-    value = value + 2;
+    value = value + 2
+    
     // Observar el resultado
-    if (value > 10) {
-      // TODO: todo bien
-    } else {
-      throw `${value} no es mayor a 10`;
-    }
+    expect(value).toBeGreaterThan(10)
+
   });
+
 });
