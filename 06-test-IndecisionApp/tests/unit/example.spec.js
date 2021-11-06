433d8b1c331b67816086ee1ca2eba89f5a8bc77a
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2021-11-06 12:16:04 +0100

    the first unit test component faillure

--- 06-test-IndecisionApp/tests/unit/example.spec.js
+++ 06-test-IndecisionApp/tests/unit/example.spec.js
@@ -1,12 +1,15 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
-
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+describe("example Component", () => {
+  test("Debe de ser > a 10", () => {
+    ///AAA
+    //Arreglar
+    const value = 5;
+    // Estimulo
+    value = value + 2;
+    // Observar el resultado
+    if (value > 10) {
+      // TODO: todo bien
+    } else {
+      throw `${value} no es mayor a 10`;
+    }
+  });
+});
