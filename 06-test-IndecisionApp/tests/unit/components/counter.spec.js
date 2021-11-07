a321878f879c5d22b0935fb0630a9db97e969419
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2021-11-07 22:47:15 +0100

    subiendo test:unit Counter find & findAll

--- 06-test-IndecisionApp/tests/unit/components/counter.spec.js
+++ 06-test-IndecisionApp/tests/unit/components/counter.spec.js
@@ -2,23 +2,36 @@ import { shallowMount , mount } from '@vue/test-utils'
 import Counter from '@/components/Counter'
 
 describe('Counter component', ()=>{
+    let wrapper;
+    beforeEach(()=>{
+        wrapper = shallowMount(Counter)
+    })
     
-    //test('Debe de hacer identico cpn el snapshot ', () => {
-    //    const wrapper = shallowMount(Counter)
-    //        expect(wrapper.html()).toMatchSnapshot()
-    //})
 
     test('h2 debe tener el valor por defecto "Counter" ', () => {
-        const wrapper = shallowMount ( Counter )
         expect(wrapper.find('h2').exists()).toBe(true)
         const h2Value = wrapper.find('h2').text()
         expect(h2Value).toBe('Counter')
     })
 
     test('el valor por defecto debe de ser 100 en el "p" ', () => {
-        const wrapper = shallowMount ( Counter )
-        //const pTags = wrapper.findAll('p')
         const value = wrapper.find('[data-testid="counter"]').text()
         expect(value).toBe("100")
     })
+
+    test('debe de incrementar y decrementar en 1 el valor del contador ', async () => {
+        
+        const[increseBtn , drecreaseBtn ] = wrapper.findAll('button')
+        await increseBtn.trigger('click')
+        await increseBtn.trigger('click')
+        await increseBtn.trigger('click')
+        
+        await drecreaseBtn.trigger('click')
+        await drecreaseBtn.trigger('click')
+       
+        const value = wrapper.find('[data-testid="counter"]').text()
+        expect (value).toBe('101')
+        
+    })
+    
 })
