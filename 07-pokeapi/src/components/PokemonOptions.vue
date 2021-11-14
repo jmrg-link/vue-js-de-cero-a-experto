f0613adc890c19a412a879441f2d188cf757510a
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2021-11-14 22:08:11 +0100

    Sc7: terminado a la espera del testing en la Sc8

--- 07-pokeapi/src/components/PokemonOptions.vue
+++ 07-pokeapi/src/components/PokemonOptions.vue
@@ -1,36 +1,44 @@
 <template >
     <div class="options-container">
         <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
+            <li v-for="pokemon in pokemons" :key="pokemon.id" @click="$emit('selection',pokemon.id)">{{ pokemon.name }}</li>
         </ul>
     </div>
 </template>
+
 <script>
-export default {};
+export default {
+  props:{
+    pokemons:{
+      type:Array,
+      required:true
+    }
+  }
+}
 </script>
+
 <style scoped>
 /* Pokemon Options */
 ul {
-  list-style-type: none;
+    list-style-type: none;
 }
 li {
-  background-color: white;
-  border-radius: 5px;
-  border: 1px solid rgba(0, 0, 0, 0.2);
-  cursor: pointer;
-  margin-bottom: 10px;
-  width: 250px;
+    background-color: white;
+    border-radius: 6px;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    cursor: pointer;
+    padding:5px;
+    margin-bottom: 10px;
+    width: 375px;
+    font-size:18px;
 }
 
 li:hover {
-  background-color: rgba(0, 0, 0, 0.05);
+    background-color: rgba(0, 0, 0, 0.05);
 }
 
 .options-container {
-  display: flex;
-  justify-content: center;
+    display: flex;
+    justify-content: center;
 }
 </style>
--- 07-pokeapi/src/components/PokemonPicture.vue
+++ 07-pokeapi/src/components/PokemonPicture.vue
@@ -1,13 +1,37 @@
 import Counter from '@/components/Counter';
 <template lang="">
-<div class="pokemon-container">      
-    <img class="fade-in" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="pokemon">
-    <img class="hidden-pokemon" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="pokemon">
-</div>
+  <div class="pokemon-container">
+    <img
+      v-if="!showPokemon"
+      :src="imgSrc"
+      class="hidden-pokemon"
+      alt="pokemon"
+    />
+
+    <img v-else :src="imgSrc" class="fade-in" alt="pokemon" />
+  </div>
 </template>
 <script>
-export default {};
+export default {
+  props: {
+    pokemonId: {
+      type: Number,
+      required: true,
+    },
+    showPokemon: {
+      type: Boolean,
+      required: true,
+      defaulut: false,
+    },
+  },
+  computed: {
+    imgSrc() {
+      return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${this.pokemonId}.svg`;
+    },
+  },
+};
 </script>
+
 <style scoped>
 /* Pokemon Picture */
 .pokemon-container {
@@ -16,7 +40,8 @@ export default {};
 img {
   height: 200px;
   position: absolute;
-  right: 32%;
+  width: 100vw;
+  margin-left: -50vw;
   user-select: none;
   -moz-user-select: none;
   -ms-user-select: none;
