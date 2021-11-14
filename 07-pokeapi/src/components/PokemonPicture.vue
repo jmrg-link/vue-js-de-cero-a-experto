1152a6b343c2b1a91b078d5f6be07363cbf80e64
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2021-11-14 02:30:09 +0100

    añadiendo pokeapi

---
+++ 07-pokeapi/src/components/PokemonPicture.vue
@@ -0,0 +1,29 @@
+import Counter from '@/components/Counter';
+<template lang="">
+<div class="pokemon-container">      
+    <img class="fade-in" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="pokemon">
+    <img class="hidden-pokemon" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="pokemon">
+</div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+/* Pokemon Picture */
+.pokemon-container {
+  height: 200px;
+}
+img {
+  height: 200px;
+  position: absolute;
+  right: 32%;
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
+.hidden-pokemon {
+  filter: brightness(0);
+}
+</style>
---
+++ 07-pokeapi/src/pages/PokemonPage.vue
@@ -0,0 +1,18 @@
+<template lang="">
+    <div>
+        <h1>¿Quien es este pokemon?</h1>
+        <!--TODO: img --><PokemonPicture />
+        <!--TODO: opciones --><PokemonOptions />
+    </div>
+</template>
+<script>
+import PokemonOptions from '@/components/PokemonOptions'
+import PokemonPicture from '@/components/PokemonPicture'
+
+export default {
+    components: {
+        PokemonPicture,
+        PokemonOptions
+    }
+}
+</script>
