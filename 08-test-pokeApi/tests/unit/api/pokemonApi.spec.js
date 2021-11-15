b63c3afe1f76cb8e08996197ed9573024254ab92
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2021-11-15 01:37:20 +0100

    añadir pruebas unitarias sobre endpoint pokeApi

---
+++ 08-test-pokeApi/tests/unit/api/pokemonApi.spec.js
@@ -0,0 +1,8 @@
+import pokemonApi from '@/api/pokemonapi'
+describe('pokemonApi', () => {
+    test('axios debe estar configurado con el api de pokemon ', () => {
+        //console.log(pokemonApi)
+        expect(pokemonApi.defaults.baseURL).toBe('https://pokeapi.co/api/v2/pokemo')
+    })
+    
+})
