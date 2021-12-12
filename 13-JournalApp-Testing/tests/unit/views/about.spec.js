0dc48a85f124d93e6578f674b5e976b7250ca669
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2021-12-12 10:34:10 +0100

    Sc13: testing vuex

---
+++ 13-JournalApp-Testing/tests/unit/views/about.spec.js
@@ -0,0 +1,8 @@
+import { shallowMount } from "@vue/test-utils";
+improt about from "@/tests/unit/views/about.spec.j"
+
+describe("Pruebas en el About.vue", () => {
+
+
+    test('Debe de renderizar el componente correctamente')
+})
