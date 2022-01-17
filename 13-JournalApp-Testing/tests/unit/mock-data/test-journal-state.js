35986c1c2b54c01d65f0d3b9c9ca7c213ee3052d
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2022-01-17 21:24:20 +0100

    preparando el store para testing con jest

---
+++ 13-JournalApp-Testing/tests/unit/mock-data/test-journal-state.js
@@ -0,0 +1,21 @@
+export const journalState = {
+    isLoading: false,
+    entries: [
+        {
+            id: "-MqU3Rj_sP6Rbm1RS2PG",
+            date: 1639051274464,
+            text: "nueva entrada..."
+        },
+        {
+            id: "-MqUePtgfaib9tio3XUF",
+            date: 1639061218733,
+            picture: "https://res.cloudinary.com/dqhfairbx/image/upload/v1639063054/iou33dhvmulfbegxl1g7.jpg",
+            text: "no hay nada dentro"
+        },
+        {
+            id: "-MrJ22mV-o4TRSjomhpy",
+            date: "Fry Dec 19 2021",
+            text: "crear nueva entrada finalizado"
+        }
+    ]
+}
--- 13-JournalApp-Testing/tests/unit/modules/daybook/store/journal/journal-module.spec.js
+++ 13-JournalApp-Testing/tests/unit/modules/daybook/store/journal/journal-module.spec.js
@@ -1,7 +1,29 @@
+import {createStore} from "vuex";
+import journal from '@/modules/daybook/store/journal'
+import {journalState} from "../../../../mock-data/test-journal-state";
 
-describe('Vuex - Pruebas en el Journal Module', ()=> {
+const createVuexStore = (initialState) => createStore({
+    modules: {
+        journal: {
+            ...journal, state: {...initialState}
+        }
+    }
+})
+
+describe('Vuex - Este es el state inicial', () => {
     //basics
-    test('should', () => {
+    test('este es el estado inicial, debe de tener este state', () => {
+        const store = createVuexStore(journalState)
+        const {isLoading, entries} = store.state.journal
+        expect(isLoading).toBeFalsy()
+        expect(entries).toEqual(journalState.entries)
+    })
 
+    // mutations
+    test('mutation: setEntries', () => {
+        const store = createVuexStore({isLoading: true, entries: []})
+        store.commit('journal/setEntries', journalState.entries) // arreglo de test-journal-state
+        expect(store.state.journal.entries.length).toBe(3)// recorre todos los objetos de entries en test-journal-state
+        expect(store.state.journal.entries.isLoading).toBeFalsy() // isLoading tiene que ser false
     })
 })
