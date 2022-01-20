dc2fd0153d76470d40e0c3c4b4202a27e814d869
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2022-01-20 22:13:21 +0100

    finalizando test con vuex en el api de firebase

--- 13-JournalApp-Testing/src/modules/daybook/store/journal/actions.js
+++ 13-JournalApp-Testing/src/modules/daybook/store/journal/actions.js
@@ -1,14 +1,14 @@
 import journalApi from "@/api/journalApi";
 
-export const loadEntries = async ({ commit }) => {
-    const { data } = await journalApi.get('/entries.json')
-    if(!data){
-        commit('setEntries',[])
+export const loadEntries = async ({commit}) => {
+    const {data} = await journalApi.get('/entries.json')
+    if (!data) {
+        commit('setEntries', [])
         return
     }
 
     const entries = []
-    for( let id of Object.keys(data) ){
+    for (let id of Object.keys(data)) {
         entries.push({
             id,
             ...data[id]
@@ -17,25 +17,27 @@ export const loadEntries = async ({ commit }) => {
     //console.log(entries)
     commit('setEntries', entries)
 }
-export const updateEntry = async ({ commit }, entry) => {
-    const { date , picture , text } = entry
-    const dataToSave = {date , picture , text }
-    const resp = await journalApi.put( `/entries/${entry.id}.json`,dataToSave)
-    console.log(resp)
-    commit('updateEntry',{ ...entry })
+export const updateEntry = async ({commit}, entry) => { // entry debe ser un parametro
+    const {date, picture, text} = entry
+    const dataToSave = {date, picture, text}
+    const resp = await journalApi.put(`/entries/${entry.id}.json`, dataToSave)
+    dataToSave.id = entry.id // dentro de dataToSave tiene que ser igual que el contenido del object de entry.id
+    // console.log(resp)
+    // commit de una mutation -> updateEntry
+    commit('updateEntry', {...dataToSave})
 }
 
-export const createEntry = async ({ commit }, entry) => {
-    const { date , picture , text } = entry
-    const dataToSave = { date , picture , text }
-    const { data } = await journalApi.post(`/entries.json`,dataToSave)
+export const createEntry = async ({commit}, entry) => {
+    const {date, picture, text} = entry
+    const dataToSave = {date, picture, text}
+    const {data} = await journalApi.post(`/entries.json`, dataToSave)
     dataToSave.id = data.name
-    commit('addEntry',dataToSave)
+    commit('addEntry', dataToSave)
     return data.name
 }
 
-export const deleteEntry = async ( {commit}, id ) => {
+export const deleteEntry = async ({commit}, id) => {
     await journalApi.delete(`/entries/${id}.json`)
-    commit('deleteEntry',id)
+    commit('deleteEntry', id)
     return id
 }
--- 13-JournalApp-Testing/tests/unit/modules/daybook/store/journal/journal-module.spec.js
+++ 13-JournalApp-Testing/tests/unit/modules/daybook/store/journal/journal-module.spec.js
@@ -26,6 +26,7 @@ describe('Vuex - Pruebas en el Journal Module', () => {
         expect(store.state.journal.entries.length).toBe(3)// recorre todos los objetos de entries en test-journal-state
         expect(store.state.journal.entries.isLoading).toBeFalsy() // isLoading tiene que ser false
     })
+
     //Mutations : updateEntries =========================================>
     test("mutations: updateEntrie", () => {
         const store = createVuexStore(journalState)
@@ -39,6 +40,7 @@ describe('Vuex - Pruebas en el Journal Module', () => {
         expect(store.state.journal.entries.length).toBe(3)
         expect(storeEntries.find(e => e.id === updatedEntry.id)).toEqual(updatedEntry)
     })
+
     // Mutations : addEntry + deleteEntry =========================================>
     test("mutations: addEntry y deleteEntry", () => {
         const store = createVuexStore(journalState)
@@ -54,14 +56,64 @@ describe('Vuex - Pruebas en el Journal Module', () => {
         expect(delEntries.length).toBe(3)
         expect(delEntries.find(e => e.id === 'ABC-123')).toBeFalsy()
     })
+
     // Getters ================================>
     test("getters: getentriesByTerm && getEntryById", () => {
         const store = createVuexStore(journalState)
         const [entry1, entry2, entry3] = journalState.entries
-        expect(store.getters['journal/getEntriesByTerm']('').length).toBe(3)
-        expect(store.getters['journal/getEntriesByTerm']('finalizado').length).toBe(1)
-        expect(store.getters['journal/getEntriesByTerm']('finalizado')).toEqual([entry3])
+        expect(store.getters['journal/getEntriesByTerm']('').length).toBe(3)// recorre las entradas - deben tener 3
+        expect(store.getters['journal/getEntriesByTerm']('finalizado').length).toBe(1) // recorre las entradas , encuentra palabra finalizado que existe solo en 1 objeto
+        expect(store.getters['journal/getEntriesByTerm']('finalizado')).toEqual([entry3]) // destruct arr3 y tiene que ser igual a finalizado
+        expect(store.getters['journal/getEntryById']('-MqU3Rj_sP6Rbm1RS2PG')).toEqual(entry1) // destruct arr entry 1 con object :id igual al introducido
+    })
 
-        expect(store.getters['journal/getEntryById']('-MqU3Rj_sP6Rbm1RS2PG')).toEqual(entry1)
+    // Actions ================================>
+    test("actions: loadEntries", async () => {
+        const store = createVuexStore({isLoading: true, entries: []})
+        await store.dispatch('journal/loadEntries')
+        const storeState = store.state.journal.entries
+        expect(storeState.length).toBe(4) // Comprueba que existen 4 entries en firebase, devuelve true si es correcto y pasa el test
+    })
+
+    test("actions: updateEntry", async () => {
+        const store = createVuexStore(journalState)
+        const updatedEntry = {
+            id: '-MqU3Rj_sP6Rbm1RS2PG',
+            date: 1639051274464,
+            picture: 'https://res.cloudinary.com/dqhfairbx/image/upload/v1639063054/iou33dhvmulfbegxl1g7.jpg',
+            text: "nueva entrada 200122...",
+            nextCamp: true,
+            olderCamp: {test: 1}
+        }
+        await store.dispatch('journal/updateEntry', updatedEntry)
+        const storeState = store.state.journal.entries
+        expect(storeState.length).toBe(3)
+        expect(storeState.find(e => e.id === updatedEntry.id)).toEqual({
+            id: '-MqU3Rj_sP6Rbm1RS2PG',
+            date: 1639051274464,
+            text: "nueva entrada 200122...",
+            picture: 'https://res.cloudinary.com/dqhfairbx/image/upload/v1639063054/iou33dhvmulfbegxl1g7.jpg'
+        })
+    })
+
+    test("actions: createEntry & deleteEntry", async () => {
+        const store = createVuexStore(journalState)
+
+        // createEntry
+        const newEntry = {
+            date: 1639051274464,
+            text: "nuevo test de creacion de entrada 200122...",
+            picture:'',
+        }
+        const id = await store.dispatch('journal/createEntry', newEntry)
+        expect(typeof id).toBe('string')
+        const copyEntries = store.state.journal.entries
+        expect(store.state.journal.entries.find(e => e.id === id)).toBeTruthy()
+
+        // deleteEntry
+        // borramos el mismo id que creamos anteriormente
+        await store.dispatch('journal/deleteEntry', id)
+        const delEntries = store.state.journal.entries
+        expect(delEntries.find(e => e.id === id)).toBeFalsy()
     })
 })
