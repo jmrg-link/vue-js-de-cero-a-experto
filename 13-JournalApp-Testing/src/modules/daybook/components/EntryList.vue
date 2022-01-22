c473202460b078e97da70a3b7893ea25a614b3a0
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2022-01-22 16:58:47 +0100

    jest entryList finalizado

--- 13-JournalApp-Testing/src/modules/daybook/components/EntryList.vue
+++ 13-JournalApp-Testing/src/modules/daybook/components/EntryList.vue
@@ -1,60 +1,61 @@
 <template>
-<div class="entry-list-container">
-  <div class="px-2 pt-2">
-    <input
-        type="text"
-        class="form-control"
-        placeholder="Buscar entrada"
-        v-model="term"
-    />
-  </div>
+    <div class="entry-list-container">
+        <div class="px-2 pt-2">
+            <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Buscar entrada"
+                    v-model="term"
+            />
+        </div>
 
-  <div class="mt-2 d-flex flex-column">
-  <button class="btn btn-primary mx-5">
-    <i class="fa fa-plus-circle"
-       @click="$router.push({name:'entry', params: { id: 'new' }})">
-      Nueva entrada
-    </i>
-  </button>
-  </div>
+        <div class="mt-2 d-flex flex-column">
+            <button class="btn btn-primary mx-5">
+                <i class="fa fa-plus-circle"
+                   @click="$router.push({name:'entry', params: { id: 'new' }})">
+                    Nueva entrada
+                </i>
+            </button>
+        </div>
 
-  <div class="entry-scrollablea-area">
-    <Entry
-    v-for="entry in entriesByTerm"
-    :key="entry.id"
-    :entry="entry"
-    />
-  </div>
-</div>
+        <div class="entry-scrollablea-area">
+            <Entry
+                    v-for="entry in entriesByTerm"
+                    :key="entry.id"
+                    :entry="entry"
+            />
+        </div>
+    </div>
 </template>
 <script>
 import {defineAsyncComponent} from "vue";
-import { mapGetters } from 'vuex'
+import {mapGetters} from 'vuex'
 
 export default {
-  components:{
-   Entry: defineAsyncComponent(()=> import('./Entry') ),
-  },
-  computed:{
-    ...mapGetters('journal',['getEntriesByTerm']),
-    entriesByTerm(){
-      return this.getEntriesByTerm(this.term)
+    components: {
+        Entry: defineAsyncComponent(() => import('./Entry')),
+    },
+    computed: {
+        ...mapGetters('journal', ['getEntriesByTerm']),
+        entriesByTerm() {
+            return this.getEntriesByTerm(this.term)
+        }
+    },
+    data() {
+        return {
+            term: ''
+        }
     }
-  },
-  data(){
-    return {
-      term:''
-    }
-  }
 }
 </script>
 <style lang="scss" scoped>
-.entry-list-container{
+.entry-list-container {
   border-right: 1px solid #2c3e50;
-  height: calc( 100vh - 56px );
+  height: calc(100vh - 56px);
 }
-.entry-scrollablea-area{
-  height: calc( 100vh - 110px );
+
+.entry-scrollablea-area {
+  height: calc(100vh - 110px);
   overflow: scroll;
 }
 </style>
