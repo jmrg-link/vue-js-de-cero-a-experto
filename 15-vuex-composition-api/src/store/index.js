b4ca7704c0d34e91eec8af19f08de1bc1646e4eb
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2022-02-07 21:18:51 +0100

    compositionApi + vuex : {computed todoApp}

--- 15-vuex-composition-api/src/store/index.js
+++ 15-vuex-composition-api/src/store/index.js
@@ -1,12 +1,44 @@
 import { createStore } from 'vuex'
 
-export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+export default createStore( {
+    state: {
+        todos: [
+            { id: '1' , text: 'Recolectar priedad del infinito' , completed: false } ,
+            { id: '2' , text: 'Piedra del Alma' , completed: true } ,
+            { id: '3' , text: 'Piedra del Poder' , completed: true } ,
+            { id: '4' , text: 'Piedra de la realidad' , completed: false } ,
+            { id: '5' , text: 'Conseguir nuevos secuaces' , completed: false } ,
+        ]
+    } ,
+    mutations: {
+        toggleTodo( state , id ) {
+            const todoIdx = state.todos.findIndex( todo => todo.id === id )
+            //console.log( todoIdx )
+            state.todos[todoIdx].completed = !state.todos[todoIdx].completed
+        }
+    } ,
+    actions: {} ,
+    getters: {
+        pendingTodos( state , getters , rootState ) {
+            return state.todos.filter( todo => !todo.completed )
+        } ,
+        allTodos: ( state , getters , rootState ) => {
+            //console.log( { state , getters , rootState } )
+            return state.todos
+        } ,
+        completedTodos: ( state , getters , rootState ) => {
+            return state.todos.filter( todo => todo.completed )
+        } ,
+        getTodosByTab: ( _ , getters , rootState ) => ( tab ) => {
+            switch ( tab ) {
+                case 'all':
+                    return getters.allTodos
+                case 'pending':
+                    return getters.pendingTodos
+                case 'completed':
+                    return getters.completedTodos
+            }
+        }
+    } ,
+    modules: {}
+} )
--- 15-vuex-composition-api/src/views/TodoVuex.vue
+++ 15-vuex-composition-api/src/views/TodoVuex.vue
@@ -1,15 +1,82 @@
 <template>
   <h1>Thanos List 📚:</h1>
+  <!--<h4>Tareas 📑: {{ $store.state.todos.length }}</h4>-->
+  <h4>Tareas pendientes 📑:{{ pending.length }}</h4>
+  <!--<p>{{ pending }}</p>-->
+  <hr>
+  <!--<h4>Todos 📑:{{ all.length }}</h4>-->
+  <!--<h4>Completados 📑:{{ completed.length }}</h4>-->
+  <button
+      :class="{'active' : currentTab ==='all'}"
+      @click="currentTab = 'all'"
+  >Todos
+  </button>
+  <button
+      :class="{'active' : currentTab ==='pending'}"
+      @click="currentTab = 'pending'"
+  >Pendientes
+  </button>
+  <button
+      :class="{'active' : currentTab ==='completed'}"
+      @click="currentTab = 'completed'"
+  >Completados
+  </button>
+
+  <div>
+    <ul>
+      <li v-for="todo in getTodosByTab" :key="todo.id"
+          :class="{'completed' : todo.completed === true}"
+          @dblclick="toggleTodo(todo.id)">
+        {{ todo.text }}
+      </li>
+    </ul>
+  </div>
+
 </template>
 <script>
-import { ref } from 'vue'
+import { computed , ref } from 'vue'
+import { useStore } from 'vuex'
 
 export default {
   setup() {
+    const store = useStore()
 
+    const currentTab = ref( 'all' )
+
+    return {
+      currentTab ,
+      pending: computed( () => store.getters['pendingTodos'] ) ,
+      all: computed( () => store.getters['allTodos'] ) ,
+      completed: computed( () => store.getters['completedTodos'] ) ,
+      getTodosByTab: computed( () => store.getters['getTodosByTab']( currentTab.value ) ) ,
+      toggleTodo: ( id ) => store.commit( 'toggleTodo' , id ) ,
+    }
   }
 }
 </script>
 <style scoped>
+div {
+  display: flex;
+  justify-content: center;
+  text-align: center
+}
 
+ul {
+  list-style: none;
+  width: 300px;
+  text-align: left;
+}
+
+li {
+  cursor: pointer;
+}
+
+.active {
+  background-color: #2c3e50;
+  color: white;
+}
+
+.completed {
+  text-decoration: line-through;
+}
 </style>
