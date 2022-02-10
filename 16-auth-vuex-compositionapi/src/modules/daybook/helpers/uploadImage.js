c501f9969ac8c52d8a4285395482af4dd4608be4
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2022-02-10 01:07:34 +0100

    configurando sc16

--- 16-auth-vuex-compositionapi/src/modules/daybook/helpers/uploadImage.js
+++ 16-auth-vuex-compositionapi/src/modules/daybook/helpers/uploadImage.js
@@ -1,28 +1,18 @@
-import axios from 'axios'
-
+import cloudinaryImage from '@/api/cloudinaryImage'
 
 const uploadImage = async ( file ) => {
-
     if ( !file ) return
-
     try {
-        
         const formData = new FormData()
-        formData.append('upload_preset','curso-vue')
-        formData.append('file', file )
-
-        const url = 'https://api.cloudinary.com/v1_1/dx0pryfzn/image/upload'
-        const { data } = await axios.post(url, formData)
-
+        formData.append( 'upload_preset' , 'vue-fh' )
+        formData.append( 'file' , file )
+        const { data } = await cloudinaryImage.post( cloudinaryImage.baseURL , formData )
+        console.log( data )
         return data.secure_url
-
-    } catch (error) {
-        console.error('Error al cargar la imagen, revisar logs')
-        console.log(error)
+    } catch ( e ) {
+        console.error( 'Error al cargar la Imagen' , e )
         return null
     }
-
-
 }
 
 export default uploadImage
